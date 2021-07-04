82942615aa027549de0f367f8aab3e846b42c453
lotcarnage <lotcarnage@gmail.com>
2021-07-04 17:29:04 +0900

    [fix] clang++でCソースファイルをコンパイルする事が非推奨になっているため、clangでコンパイルするように修正しました。

--- c/performance_test/mq_peformance/build/macos/sh/build.sh
+++ c/performance_test/mq_peformance/build/macos/sh/build.sh
@@ -1,2 +1,4 @@
-clang++ -c ../../../code/compare_mq_qsq.cpp ../../../code/random_numbers.c -I ../../../code/ -I ../../../../../container/ ../../../../../container/median_queue.c ../../../../../container/qs_queue.c
+clang -O3 -c ../../../../../container/median_queue.c ../../../../../container/qs_queue.c -I ../../../../../container/
+clang -O3 -c ../../../code/random_numbers.c -I ../../../code/
+clang++ -O3 -c ../../../code/compare_mq_qsq.cpp -I ../../../code/ -I ../../../../../container/
 ld compare_mq_qsq.o median_queue.o qs_queue.o random_numbers.o -lc++ -lc
