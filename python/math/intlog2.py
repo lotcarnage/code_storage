1cc1d32c1beac0beb36ee4dc7481295bd8ee2e2b
lotcarnage <lotcarnage@gmail.com>
2021-07-22 01:09:29 +0900

    [fix] bin_length系の実装不具合を修正しました。 [update] スタンフォード大学のWebサイト上で公開されていた実装を追加しました。 [update] テストを追加しました。

--- python/math/intlog2.py
+++ python/math/intlog2.py
@@ -128,18 +128,73 @@ def intlog2_16bit_table(v):
         return bit16_table[v]
 
 def intlog2_py_string(v):
-    return len(bin(v)) - 2
+    return len(bin(v)) - 3
 
 def intlog2_py_bit_length(v):
-    return v.bit_length()
+    return v.bit_length() - 1
 
+BIT_LENGTH_TABLE = [
+    0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+]
 def intlog2_py_bit_length_on_python(v):
     msb = 0;
     while (v >= 32):
         msb +=6;
         v >>= 6;
-    msb += bit8_table[v]
-    return msb
+    msb += BIT_LENGTH_TABLE[v]
+    return msb - 1
+
+# http://graphics.stanford.edu/~seander/bithacks.html#IntegerLog
+# Find the log base 2 of an integer with a lookup table
+def intlog2_stanford_with_lookup_table(v):
+    if (tt := v >> 16):
+        r = 24 + bit8_table[t] if (t := tt >> 8) else 16 + bit8_table[tt];
+    else:
+        r = 8 + bit8_table[t] if (t := v >> 8) else bit8_table[v];
+    return r
+
+# Find the log base 2 of an N-bit integer in O(lg(N)) operations 
+LG_N_BITMASK = [0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000]
+LG_N_SHIFT = [1, 2, 4, 8, 16]
+def intlog2_stanford_O_lg_N_operations_1(v):
+    r = 0
+    for i in range(4, -1, -1):
+        if (v & LG_N_BITMASK[i]):
+            v >>= LG_N_SHIFT[i];
+            r |= LG_N_SHIFT[i];
+    return r
+
+def intlog2_stanford_O_lg_N_operations_2(v):
+    r = (v > 0xFFFF) << 4; v >>= r;
+    shift = (v > 0xFF  ) << 3; v >>= shift; r |= shift;
+    shift = (v > 0xF   ) << 2; v >>= shift; r |= shift;
+    shift = (v > 0x3   ) << 1; v >>= shift; r |= shift;
+    r |= (v >> 1);
+    return r
+
+# Find the log base 2 of an N-bit integer in O(lg(N)) operations with multiply and lookup 
+MultiplyDeBruijnBitPosition = [
+    0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+    8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+]
+def intlog2_stanford_O_lg_N_operations_with_multiply_and_lookup(v):
+    v |= v >> 1
+    v |= v >> 2
+    v |= v >> 4
+    v |= v >> 8
+    v |= v >> 16
+    return MultiplyDeBruijnBitPosition[((v * 0x07C4ACDD) % 0xffffffff) >> 27]
+
+def intlog2_test(target_function, expected_function, range_):
+    is_succeeded = True
+    for v in range_:
+        actual = target_function(v)
+        expected = expected_function(v)
+        if actual != expected:
+            print(target_function.__name__, v, actual, expected)
+            is_succeeded = False
+    return is_succeeded
 
 if __name__ == "__main__":
     import time
@@ -150,15 +205,33 @@ if __name__ == "__main__":
         et = time.time() - st
         return (target_function.__name__, et, _)
 
-    results = []
-    results.append(_time(intlog2_math))
-    results.append(_time(intlog2_linsearch))
-    results.append(_time(intlog2_binsearch))
-    results.append(_time(intlog2_8bit_table))
-    results.append(_time(intlog2_16bit_table))
-    results.append(_time(intlog2_py_string))
-    results.append(_time(intlog2_py_bit_length))
-    results.append(_time(intlog2_py_bit_length_on_python))
+    intlog2_functions = [
+        intlog2_math,
+        intlog2_linsearch,
+        intlog2_binsearch,
+        intlog2_8bit_table,
+        intlog2_16bit_table,
+        intlog2_py_string,
+        intlog2_py_bit_length,
+        intlog2_py_bit_length_on_python,
+        intlog2_stanford_with_lookup_table,
+        intlog2_stanford_O_lg_N_operations_1,
+        intlog2_stanford_O_lg_N_operations_2,
+        intlog2_stanford_O_lg_N_operations_with_multiply_and_lookup
+    ]
+    def _RED(v):
+        return f'\033[31m{v}\033[0m'
+    def _GREEN(v):
+        return f'\033[32m{v}\033[0m'
 
-    for v in results:
-        print(f"|{v[0]:20}|{v[1]:8.6f}|")
+    do_test = True
+    do_test = False
+    if do_test:
+        results = [intlog2_test(f, intlog2_py_bit_length, range(1, n)) for f in intlog2_functions]
+        result_tag = [_RED("NG"), _GREEN("OK")]
+        for result, f in zip(results, intlog2_functions):
+            print(f"{result_tag[result]}:{f.__name__}")
+    else:
+        results = [_time(f) for f in intlog2_functions]
+        for v in results:
+            print(f"|{v[0]:60}|{v[1]:8.6f}|")
